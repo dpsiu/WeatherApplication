6f25b35c80a6f3033fecee6311d11e8f6e800b8e
dpsiu <denversiu24@gmail.com>
2023-07-03 00:18:50 -0500

    Setup HTML, CSS, and JS files with base testing

---
+++ script.js
@@ -0,0 +1,17 @@
+// const apiKey = 'http://api.weatherapi.com/v1/current.json?key=098e7d365540490a963185024232706&q=' + cityParameter + '&aqi=no'
+// let cityParameter = 'austin'
+
+// console.log(apiKey)
+
+// const key = ""
+
+
+const searchBtn = document.getElementById('searchBtn')
+const cityTemp = document.getElementById('cityTemperature')
+
+async function searchCity() {
+
+    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=098e7d365540490a963185024232706&q=Austin&aqi=no")
+    const jsonData = await response.json()
+    cityTemp.innerText = jsonData.current.temp_f
+}
