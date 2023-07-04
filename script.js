f59580f7409aceb6b5322b8bd00f1be75d37f8e2
dpsiu <denversiu24@gmail.com>
2023-07-04 14:21:04 -0500

    Add feature - weather main info js
    Linked all appropriate api elements to respective data, default Aus, TX

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-// const apiKey = 'http://api.weatherapi.com/v1/current.json?key=098e7d365540490a963185024232706&q=' + cityParameter + '&aqi=no'
-// let cityParameter = 'austin'
-
-// console.log(apiKey)
-
-// const key = ""
-
+let city = "Austin"
+let apiKey = "http://api.weatherapi.com/v1/current.json?key=098e7d365540490a963185024232706&q=" + city + "&aqi=no"
 
 const searchBtn = document.getElementById('searchBtn')
 const cityTemp = document.getElementById('cityTemperature')
+const cityState = document.getElementById('cityState')
+const weatherStat = document.getElementById('weatherStatus')
+const humidity = document.getElementById('humidity')
+const visibility = document.getElementById('visibility')
+const uv = document.getElementById('uv')
+const windDir = document.getElementById('windDir')
+const windMPH = document.getElementById('windMPH')
+const precip = document.getElementById('precip')
+
 let degreeSymbol = '\u00B0'
 
-async function searchCity() {
+renderData()
 
-    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=098e7d365540490a963185024232706&q=Austin&aqi=no")
+async function renderData() {
+
+    const response = await fetch(apiKey)
     const jsonData = await response.json()
-    cityTemp.innerText = jsonData.current.temp_f + degreeSymbol;
+    renderMiscData()
 }
+
+async function renderMiscData() {
+    const response = await fetch(apiKey)
+    const jsonData = await response.json()
+    cityState.innerText = jsonData.location.name + ', ' + jsonData.location.region
+    cityTemp.innerText = jsonData.current.temp_f + degreeSymbol;
+    weatherStat.innerText = jsonData.current.condition.text
+    humidity.innerText = jsonData.current.humidity + '%'
+    visibility.innerText = jsonData.current.vis_miles + ' mi'
+    uv.innerText = jsonData.current.uv
+    windDir.innerText = jsonData.current.wind_dir
+    windMPH.innerText = jsonData.current.wind_mph + ' mph'
+    precip.innerText = jsonData.current.precip_in + ' in'
+}
+
