780f0e2d2cb8c59c457d286b0dd7c8751b0ba1e5
dpsiu <denversiu24@gmail.com>
2023-07-03 23:28:04 -0500

    Add feature - main weather info html/css

--- script.js
+++ script.js
@@ -8,10 +8,11 @@
 
 const searchBtn = document.getElementById('searchBtn')
 const cityTemp = document.getElementById('cityTemperature')
+let degreeSymbol = '\u00B0'
 
 async function searchCity() {
 
     const response = await fetch("http://api.weatherapi.com/v1/current.json?key=098e7d365540490a963185024232706&q=Austin&aqi=no")
     const jsonData = await response.json()
-    cityTemp.innerText = jsonData.current.temp_f
+    cityTemp.innerText = jsonData.current.temp_f + degreeSymbol;
 }
